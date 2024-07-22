56327028179e748ea6fe4abf5a1337c73108c444
Yegor Bugayenko <yegor256@gmail.com>
2024-07-22 22:47:56 +0300

    largetext

--- front/front_helpers.rb
+++ front/front_helpers.rb
@@ -23,8 +23,21 @@
 # SOFTWARE.
 
 require 'tago'
+require 'cgi'
 
 helpers do
+  def largetext(text)
+    span = "<span style='display:inline-block;'>"
+    body = CGI.escapeHTML(text)
+      .tr("\n", '↵')
+      .split(/(.{4})/)
+      .map { |i| i.gsub(' ', "<span class='lightgray'>&#x2423;</span>") }
+      .join("</span>#{span}")
+      .chars.map { |c| c.ord > 0x7f ? "<span class='firebrick'>\\x#{format('%x', c.ord)}</span>" : c }
+      .join
+    "#{span}#{body}</span>"
+  end
+
   def ago(time)
     "<span title='#{time.utc.iso8601}'>#{time.ago} ago</span>"
   end
