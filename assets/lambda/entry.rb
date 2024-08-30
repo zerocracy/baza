76ad586e3ebd5a3b42e5cc708b79918ac390bb73
Yegor Bugayenko <yegor256@gmail.com>
2024-08-30 10:54:05 +0300

    #218: start

--- assets/lambda/entry.rb
+++ assets/lambda/entry.rb
@@ -22,7 +22,57 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-def go(x)
-  puts 'hello!'
+# {
+#   "Records": [
+#     {
+#       "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+#       "receiptHandle": "MessageReceiptHandle",
+#       "body": "Hello from SQS!",
+#       "attributes": {
+#         "ApproximateReceiveCount": "1",
+#         "SentTimestamp": "1523232000000",
+#         "SenderId": "123456789012",
+#         "ApproximateFirstReceiveTimestamp": "1523232000001"
+#       },
+#       "messageAttributes": {},
+#       "md5OfBody": "{{{md5_of_body}}}",
+#       "eventSource": "aws:sqs",
+#       "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+#       "awsRegion": "us-east-1"
+#     }
+#   ]
+# }
+
+require 'baza-rb'
+require 'loog'
+require 'aws-sdk-s3'
+require 'aws-sdk-core'
+
+def go(json, loog: Loog::VERBOSE, bucket: 'baza.zerocracy.com')
+  loog.debug("Service gem version: #{Aws::S3::GEM_VERSION}")
+  loog.debug("Core version: #{Aws::CORE_GEM_VERSION}")
+  loog.debug("Input: #{json.inspect}")
+  json['Records'].each do |rec|
+    if rec['messageAttributes']['object'].nil?
+      # get ZIP from baza /take
+      # put it to S3
+      # post a message to SQS
+    else
+      # download S3 object
+      # unzip it
+      # read list of passed swarms from JSON
+      # remove them from the unzipped archive
+      # if no swarms left
+        # delete object in S3
+        # push zip to baza /finish
+      # else
+        # remove all swarms but the first one
+        # run "judges"
+        # zip a package
+        # put it to S3 (replacing the previous one)
+        # post a message to SQS
+    end
+  end
+  Aws::S3::Client.new
   'Done!'
 end
