624620984eeae623fc956bc116a36a902b1a864c
Yegor Bugayenko <yegor256@gmail.com>
2024-09-04 11:13:11 +0300

    ops tested

--- test/baza/test_ops.rb
+++ test/baza/test_ops.rb
@@ -22,51 +22,44 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require 'securerandom'
-require_relative 'recipe'
+require 'minitest/autorun'
+require 'webmock/minitest'
+require_relative '../../objects/baza'
+require_relative '../../objects/baza/ops'
+require_relative '../test__helper'
 
-# Operations with swarms and their releases.
-#
+# Test for Ops.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
 # Copyright:: Copyright (c) 2009-2024 Yegor Bugayenko
 # License:: MIT
-class Baza::Ops
-  # Ctor.
-  #
-  # @param [Baza::Swarm] swarm The swarm
-  # @param [Loog] loog Logging facility
-  def initialize(ec2, account, id_rsa, loog: Loog::NULL)
-    @ec2 = ec2
-    @account = account
-    @id_rsa = id_rsa
-    @loog = loog
+class Baza::OpsTest < Minitest::Test
+  def test_release
+    ec2 = Baza::EC2.new(
+      'FAKEFAKEFAKEFAKEFAKE',
+      'fakefakefakefakefakefakefakefakefakefake',
+      'us-east-1',
+      'sg-424242',
+      'sn-42424242',
+      't2.large',
+      loog: Loog::NULL
+    )
+    ops = Baza::Ops.new(ec2, '', '')
+    swarm = fake_human.swarms.add(fake_name, "#{fake_name}/#{fake_name}", fake_name)
+    ops.release(swarm)
   end
 
-  # Release this swarm.
-  #
-  # @param [Baza::Swarm] swarm The swarm
-  def release(swarm)
-    secret = SecureRandom.uuid
-    instance = @ec2.run_instance(
-      "baza/#{swarm.name}",
-      Baza::Recipe.new(swarm, @id_rsa.gsub(/\n +/, "\n")).to_bash(
-        :release, @account, @ec2.region, secret
-      )
+  def test_destroy
+    ec2 = Baza::EC2.new(
+      'FAKEFAKEFAKEFAKEFAKE',
+      'fakefakefakefakefakefakefakefakefakefake',
+      'us-east-1',
+      'sg-424242',
+      'sn-42424242',
+      't2.large',
+      loog: Loog::NULL
     )
-    swarm.releases.start("Releasing AWS EC2 #{@ec2.type.inspect} instance #{instance.inspect}...", secret)
-  end
-
-  # Destroy this swarm.
-  #
-  # @param [Baza::Swarm] swarm The swarm
-  def destroy(swarm)
-    secret = SecureRandom.uuid
-    instance = @ec2.run_instance(
-      "baza/#{swarm.name}",
-      Baza::Recipe.new(swarm, @id_rsa.gsub(/\n +/, "\n")).to_bash(
-        :destroy, @account, @ec2.region, secret
-      )
-    )
-    swarm.releases.start("Destroying in EC2 #{@ec2.type.inspect} instance #{instance.inspect}...", secret)
+    ops = Baza::Ops.new(ec2, '', '')
+    swarm = fake_human.swarms.add(fake_name, "#{fake_name}/#{fake_name}", fake_name)
+    ops.destroy(swarm)
   end
 end
