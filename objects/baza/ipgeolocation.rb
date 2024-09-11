b8993453bc4c468a8bfc8efaacb9e4e7389b6935
Suban05 <anatolyb94@gmail.com>
2024-09-11 13:09:02 +0200

    #163 simplify

--- objects/baza/ipgeolocation.rb
+++ objects/baza/ipgeolocation.rb
@@ -49,99 +49,64 @@ class Baza::IpGeolocation
 
     def get(url)
       ip = URI.parse(url).query.split('&')&.last&.split('=')&.last
-      data = {
-        '8.8.8.8' => {
-          'ip' => '8.8.8.8',
-          'continent_code' => 'NA',
-          'continent_name' => 'North America',
-          'country_code2' => 'US',
-          'country_code3' => 'USA',
-          'country_name' => 'United States',
-          'country_name_official' => 'United States of America',
-          'country_capital' => 'Washington, D.C.',
-          'state_prov' => 'California',
-          'state_code' => 'US-CA',
-          'district' => '',
-          'city' => 'Mountain View',
-          'zipcode' => '94043-1351',
-          'latitude' => '37.42240',
-          'longitude' => '-122.08421',
-          'is_eu' => false,
-          'calling_code' => '+1',
-          'country_tld' => '.us',
-          'languages' => 'en-US,es-US,haw,fr',
-          'country_flag' => 'https://ipgeolocation.io/static/flags/us_64.png',
-          'geoname_id' => '6301403',
-          'isp' => 'Google LLC',
-          'connection_type' => '',
-          'organization' => 'Google LLC',
-          'country_emoji' => '🇺🇸',
-          'currency' => { 'code' => 'USD', 'name' => 'US Dollar', 'symbol' => '$' },
-          'time_zone' => {
-            'name' => 'America/Los_Angeles',
-            'offset' => -8,
-            'offset_with_dst' => -7,
-            'current_time' => '2024-09-03 07:42:32.209-0700',
-            'current_time_unix' => 1_725_374_552.209,
-            'is_dst' => true,
-            'dst_savings' => 1,
-            'dst_exists' => true,
-            'dst_start' => {
-              'utc_time' => '2024-03-10 TIME 10',
-              'duration' => '+1H',
-              'gap' => true,
-              'dateTimeAfter' => '2024-03-10 TIME 03',
-              'dateTimeBefore' => '2024-03-10 TIME 02',
-              'overlap' => false
-            },
-            'dst_end' => {
-              'utc_time' => '2024-11-03 TIME 09',
-              'duration' => '-1H',
-              'gap' => false,
-              'dateTimeAfter' => '2024-11-03 TIME 01',
-              'dateTimeBefore' => '2024-11-03 TIME 02',
-              'overlap' => true
+      GetResponse.new(
+        body: JSON.dump(
+          {
+            'ip' => ip,
+            'continent_code' => 'NA',
+            'continent_name' => 'North America',
+            'country_code2' => 'US',
+            'country_code3' => 'USA',
+            'country_name' => 'United States',
+            'country_name_official' => 'United States of America',
+            'country_capital' => 'Washington, D.C.',
+            'state_prov' => 'California',
+            'state_code' => 'US-CA',
+            'district' => '',
+            'city' => 'Mountain View',
+            'zipcode' => '94043-1351',
+            'latitude' => '37.42240',
+            'longitude' => '-122.08421',
+            'is_eu' => false,
+            'calling_code' => '+1',
+            'country_tld' => '.us',
+            'languages' => 'en-US,es-US,haw,fr',
+            'country_flag' => 'https://ipgeolocation.io/static/flags/us_64.png',
+            'geoname_id' => '6301403',
+            'isp' => ip == '8.8.8.8' ? 'Microsoft Corp' : 'Google LLC',
+            'connection_type' => '',
+            'organization' => ip == '8.8.8.8' ? 'Microsoft Company' : 'Google LLC',
+            'country_emoji' => '🇺🇸',
+            'currency' => { 'code' => 'USD', 'name' => 'US Dollar', 'symbol' => '$' },
+            'time_zone' => {
+              'name' => 'America/Los_Angeles',
+              'offset' => -8,
+              'offset_with_dst' => -7,
+              'current_time' => '2024-09-03 07:42:32.209-0700',
+              'current_time_unix' => 1_725_374_552.209,
+              'is_dst' => true,
+              'dst_savings' => 1,
+              'dst_exists' => true,
+              'dst_start' => {
+                'utc_time' => '2024-03-10 TIME 10',
+                'duration' => '+1H',
+                'gap' => true,
+                'dateTimeAfter' => '2024-03-10 TIME 03',
+                'dateTimeBefore' => '2024-03-10 TIME 02',
+                'overlap' => false
+              },
+              'dst_end' => {
+                'utc_time' => '2024-11-03 TIME 09',
+                'duration' => '-1H',
+                'gap' => false,
+                'dateTimeAfter' => '2024-11-03 TIME 01',
+                'dateTimeBefore' => '2024-11-03 TIME 02',
+                'overlap' => true
+              }
             }
           }
-        },
-        '192.168.1.1' => {
-          'ip' => '192.168.1.1',
-          'continent_code' => 'NA',
-          'continent_name' => 'North America',
-          'country_code2' => 'US',
-          'country_code3' => 'USA',
-          'country_name' => 'United States',
-          'country_name_official' => 'United States of America',
-          'country_capital' => 'Washington, D.C.',
-          'state_prov' => 'California',
-          'state_code' => 'US-CA',
-          'latitude' => '37.42240',
-          'longitude' => '-122.08421',
-          'is_eu' => false,
-          'country_flag' => 'https://ipgeolocation.io/static/flags/us_64.png',
-          'isp' => 'Microsoft Corp',
-          'organization' => 'Microsoft Company',
-        },
-        '127.0.0.1' => {
-          'ip' => '127.0.0.1',
-          'continent_code' => 'NA',
-          'continent_name' => 'North America',
-          'country_code2' => 'US',
-          'country_code3' => 'USA',
-          'country_name' => 'United States',
-          'country_name_official' => 'United States of America',
-          'country_capital' => 'Washington, D.C.',
-          'state_prov' => 'California',
-          'state_code' => 'US-CA',
-          'latitude' => '37.42240',
-          'longitude' => '-122.08421',
-          'is_eu' => false,
-          'country_flag' => 'https://ipgeolocation.io/static/flags/us_64.png',
-          'isp' => 'Microsoft Corp',
-          'organization' => 'Microsoft Company',
-        }
-      }
-      GetResponse.new(body: JSON.dump(data[ip]))
+        )
+      )
     end
   end
 end
