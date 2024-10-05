97120f68469fb6c56db743ccc513f91b59dc7dd0
Yegor Bugayenko <yegor256@gmail.com>
2024-10-06 03:48:42 +0700

    #270 test image

---
+++ test/baza/test_fake_image.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# MIT License
+#
+# Copyright (c) 2009-2024 Zerocracy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'minitest/autorun'
+require 'qbash'
+require_relative '../test__helper'
+
+# Test for Docker.
+# Author:: Yegor Bugayenko (yegor256@gmail.com)
+# Copyright:: Copyright (c) 2009-2024 Yegor Bugayenko
+# License:: MIT
+class Baza::FakeImageTest < Minitest::Test
+  def test_simple_container
+    Dir.mktmpdir do |home|
+      File.write(
+        File.join(home, 'Dockerfile'),
+        "
+        FROM ubuntu
+        WORKDIR /tmp
+        ENTRYPOINT [\"/bin/bash\", \"-c\", \"pwd\"]
+        "
+      )
+      fake_image(home) do |image|
+        stdout = qbash(
+          "docker run --rm --user #{Process.uid}:#{Process.gid} #{image}",
+          timeout: 10,
+          log: fake_loog
+        )
+        assert_equal("/tmp\n", stdout, stdout)
+      end
+    end
+  end
+end
