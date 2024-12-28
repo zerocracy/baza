9ae072d8d5f75af6e6ba943255eb989d7b9a5e67
Yegor Bugayenko <yegor256@gmail.com>
2024-12-28 16:06:51 +0300

    extra test

--- front/front_errors.rb
+++ front/front_errors.rb
@@ -78,7 +78,7 @@ error do
 end
 
 def flash(uri, msg = '', alert: false, code: 302)
-  raise "Multi-line message is a mistake: #{msg.inspect}" if /[\r\n]/ =~ msg
+  raise "Multi-line message is a mistake: #{msg.inspect}" if msg =~ /[\r\n]/
   cookies[:flash_msg] = msg
   cookies[:flash_color] = alert ? 'darkred' : 'darkgreen'
   response.headers['X-Zerocracy-Requested'] = request.url
